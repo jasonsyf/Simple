4db4a85a4f04d45f0b277975be0f65e578ccd484
syf <jason_sunyf@163.com>
2018-12-01 14:19:00 +0800

    插入排序  选择排序  快速排序  冒泡排序

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 public class Main {
-    private static int[] a = {1, 6, 5, 7, 9, 8};
+    private static int[] a = {12,1, 6,55,69,10, 5, 9 ,7, 8,25};
 
     public static void main(String[] args) {
-        SimpleSort.bubbleSort(a, a.length);
+        SimpleSort.fastSort(a, 0,a.length-1);
         System.out.println(Arrays.toString(a));
     }
 }
--- src/SimpleSort.java
+++ src/SimpleSort.java
@@ -1,3 +1,4 @@
+
 class SimpleSort {
 
     static void bubbleSort(int[] items, int count) {
@@ -19,6 +20,83 @@ class SimpleSort {
                 break;
             }
         }
+    }
+
+
+    static void insertSort(int[] items, int count) {
+        if (count <= 1) {
+            return;
+        }
+        for (int i = 1; i < count; i++) {
+            int value = items[i];
+            int j = i - 1;
+            //查找插入的位置
+            for (; j >= 0; j--) {
+                if (items[j] > value) {
+                    items[j + 1] = items[j];//数据移动
+                } else {
+                    break;
+                }
+            }
+            items[j + 1] = value;
+        }
+    }
+
+    static void selectionSort(int[] items, int count) {
+        if (count <= 1) {
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            int k = i;
+            for (int j = i + 1; j < count; j++) {
+                if (items[j] < items[k]) {
+                    k = j;
+                }
+            }
+            if (k > i) {
+                swap(items, i, k);
+            }
+        }
+//        int minPoi = 0;
+//        int i = 0;
+//        for (; i < count; i++) {
+//            int minValue = items[i];
+//            for (int j = i+1 ; j < count; j++) {
+//                if (minValue > items[j]) {
+//                    minValue = items[j];
+//                    minPoi = j;
+//                }
+//            }
+//            if (minPoi>i ) {
+//                swap(items, i, minPoi);
+//            }
+//        }
+
+    }
+
+    static void fastSort(int[] items, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int m = items[low];
+        int i = low, j = high;
+        while (i < j) {
+            while (items[j] > m && i < j) {
+                j--;
+            }
+            if (i < j) {
+                items[i++] = items[j];
+            }
+            while (items[i] < m && i < j) {
+                i++;
+            }
+            if (i < j) {
+                items[j--] = items[i];
+            }
+        }
+        items[i] = m;
+        fastSort(items, low, i - 1);
+        fastSort(items, i + 1, high);
     }
 
     private static void swap(int[] array, int x, int y) {
